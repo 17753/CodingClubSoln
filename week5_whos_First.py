3d97af6b05fd99fbf4c1371c6feab7bff717b427
17753 <61393037+17753@users.noreply.github.com>
2020-02-27 22:55:28 +1300

    Update week5_whos_First.py

--- week5_whos_First.py
+++ week5_whos_First.py
@@ -2,30 +2,38 @@ import sys
 words = []
 num = 0 
 loop = True
-#def validation():
-#   try:
-#        num = int(input())
-#        return validation()
-#    except  ValueError:
-#        num = int(input("invalid input please enter an input: "))
-#    return validation()
-num = int(input("please enter the number of words in the list: "))
-#validation()
+i = 0
+first_word = []
+def validation():
+    try:
+        num = int(input("how many words are there in the list: "))
+        return num
+    except ValueError:
+        num = int(input("Please input an valid number: "))
+num = int(input("how many words are there in the list: ")) 
 while loop:
-    if num == 0:
-        loop = false
-        print("thank you!")
-        sys.exit()
-    elif 2 < num < 100:
-        loop = False
-        for i in range(0,num,1):
-            word = str(input("please input word number " + str(i+1))) 
-            words.append(word)
-    else:
-        num = int(input("invalid input please enter an input: "))
-    
-words = sorted(words)
-print(words[0])
+   if 2 <= num <= 100:
+        loop= False
+        while i !=num:
+            i +=1
+            word = str(input("please input word number " + str(i) + ": ")) 
+            words.append(word) 
+        words = sorted(words)
+        first_word.append(words[0])
+        if i == num:
+            loop = True
+            i = 0
+            words = []
+            num = int(input("how many words are there in the list: "))
+   elif num == 0:
+            print("thank you")
+            break
+   else:
+       validation()
+for i in range(len(first_word)):
+    print(first_word[i])
+
+
                 
 
 
